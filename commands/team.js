d1b4763128a131dfd507971ae83e80e018ce0ca6
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-30 14:41:13 +0100

    Add command code file for teamsheets.

---
+++ commands/team.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'team',
+    description: 'Returns teams for next match.',
+    // eslint-disable-next-line no-unused-vars
+    execute(message, args) {
+        message.reply('Not implemented.');
+    },
+};
