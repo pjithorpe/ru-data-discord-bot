651a03e7758f009673f993e868b10cf65cf23943
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-31 22:28:59 +0100

    Add aliases for premiership and top 14 teams.

--- settings.js
+++ settings.js
@@ -3,6 +3,9 @@ const settings = {};
 settings.admin_role = '@mods';
 
 settings.team_aliases = {
+
+    // England
+    bath_rugby: 'bath',
     bears: 'bristol_bears',
     exeter_chiefs: 'exeter',
     quins: 'harlequins',
@@ -11,6 +14,35 @@ settings.team_aliases = {
     sale_sharks: 'sale',
     sarries: 'saracens',
     worcester_warriors: 'worcester',
+
+    // France
+    bordeaux: 'union_bordeaux_begles',
+    begles: 'union_bordeaux_begles',
+    bordeaux_begles: 'union_bordeaux_begles',
+    clermont: 'clermont_auvergne',
+    asm: 'clermont_auvergne',
+    lyon_olympique_universitaire: 'lyon',
+    lou: 'lyon',
+    aviron_bayonnais: 'bayonne',
+    toulon: 'rc_toulon',
+    rc_toulonnais: 'rc_toulon',
+    rugby_club_toulonnais: 'rc_toulon',
+    castres: 'castres_olympique',
+    section_paloise: 'pau',
+    su_agen_lot_et_garonne: 'agen',
+    su_agen: 'agen',
+    stade_rochelais: 'la_rochelle',
+    rochelle: 'la_rochelle',
+    stade_toulousain: 'toulouse',
+    stade_français: 'stade_francais',
+    stade_francais_paris: 'stade_francais',
+    sf: 'stade_francais',
+    racing: 'racing_92',
+    ca_brive: 'brive',
+    club_athletique_brive: 'brive',
+    club_athlétique_brive_corrèze_limousin: 'brive',
+
+    // International
     good: 'england',
     eng: 'england',
     nz: 'new_zealand',
