019a3b1d3579f1fdcca87dacd1807d11d1c3955a
Patrick Thorpe <pjithorpe@gmail.com>
2019-08-31 22:08:39 +0100

    Match command now takes a team arg and returns their next match.

--- commands/match.js
+++ commands/match.js
@@ -12,22 +12,23 @@ module.exports = {
         return sheet()
             .then(
                 (result) => {
-                    const resultRow;
-                    if (args) {
-                        let team = args[0].toLowerCase();
-                        // Check if input is an alias
-                        if(team in settings.team_aliases) team = settings.team_aliases[team];
-                        team = team.replace('_', ' ');
-                    }
-                    else {
-                        resultRow = result[0];
-                    }
-                    message.reply(
+                    // Get the first row
+                    let team = args[0].toLowerCase();
+                    // Check if input is an alias
+                    if(team in settings.team_aliases) team = settings.team_aliases[team];
+                    team = team.replace('_', ' ');
+
+                    // Find team's next match
+                    const resultRow = result.find(r => r.home.toLowerCase() === team || r.away.toLowerCase() === team);
+                    
+                    if(!resultRow) return message.reply('No match found for team \'' + team + '\'.');
+
+                    return message.reply(
                         '\n' +
-                        'Teams: ' + result[0].home + ' vs ' + result[0].away + '\n' +
-                        'Time: ' + result[0].date + ' ' + result[0].time + '\n' +
-                        'Competition: ' + result[0].competition + '\n' +
-                        'Channel: ' + result[0].channel
+                        'Teams: ' + resultRow.home + ' vs ' + resultRow.away + '\n' +
+                        'Time: ' + resultRow.date + ' ' + resultRow.time + '\n' +
+                        'Competition: ' + resultRow.competition + '\n' +
+                        'Channel: ' + resultRow.channel
                     );
                 },
                 (err) => {
--- settings.js
+++ settings.js
@@ -3,9 +3,19 @@ const settings = {};
 settings.admin_role = '@mods';
 
 settings.team_aliases = {
-    lei: 'leicester',
+    bears: 'bristol_bears',
+    exeter_chiefs: 'exeter',
+    quins: 'harlequins',
+    tigers: 'leicester',
+    irish: 'london_irish',
+    sale_sharks: 'sale',
+    sarries: 'saracens',
+    worcester_warriors: 'worcester',
+    good: 'england',
     eng: 'england',
     nz: 'new_zealand',
+    evil: 'new_zealand',
+    bokke: 'south_africa',
 };
 
-module.export = settings;
+module.exports = settings;
