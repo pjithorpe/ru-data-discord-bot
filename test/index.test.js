3402fd099f55755be189de77dd741fcbb28923c3
Patrick Thorpe <pjithorpe@gmail.com>
2019-09-30 01:52:42 +0100

    Finish tests for src/index.js fetchCommands method.

--- test/index.test.js
+++ test/index.test.js
@@ -8,10 +8,19 @@ const index = require('../src/index');
 describe('index', () => {
     const client = new Discord.Client();
     describe('#fetchCommands', () => {
-        client.commands = new Discord.Collection();
+        beforeEach(() => {
+            client.commands = new Discord.Collection();
+        });
 
         it('should throw an exception if the command file isn\'t found', () => {
             should.throw(() => { index.fetchCommands(client.commands, '/fake_directory'); }, /command/);
+        });
+
+        it('should return a collection of command names and command objects', () => {
+            client.commands = index.fetchCommands(client.commands, '/commands');
+            client.commands.should.be.an.instanceof(Discord.Collection);
+            client.commands.firstKey().should.be.a('string');
+            client.commands.first().should.be.an('object');
         });
     });
 
