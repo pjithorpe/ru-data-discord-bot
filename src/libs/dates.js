49d895000732e042160e39201db7e0d7315c4107
Patrick <pjithorpe@gmail.com>
2019-10-12 17:35:41 +0100

    Fix bug with match command not returning anything when some matches are TBC.

--- src/libs/dates.js
+++ src/libs/dates.js
@@ -3,22 +3,29 @@ const settings = require('../settings');
 
 const dates = {
     parseDateAndTime(dateStr, timeStr) {
+        // Deal with missing data
+        if (timeStr === '') timeStr = '00:00';
+
         const combinedStr = dateStr + ' ' + timeStr;
         const format = settings.date_format + ' ' + settings.time_format;
 
         const result = moment(combinedStr, format);
-        if (!result.isValid()) throw Error('Invalid date.');
+        if (!result.isValid()) return -1;
 
         return result;
     },
 
     isFutureMoment(_moment) {
-        if (_moment.isAfter(moment())) return true;
+        if (_moment === -1 || _moment.isAfter(moment())) return true;
         return false;
     },
 
     compareDates(moment1, moment2) {
-        if (moment1.isBefore(moment2)) return -1;
+        // -1 indicates a missing date
+        if (moment1 === -1 && moment2 === -1) return 0;
+        else if (moment2 === -1) return -1;
+        else if (moment1 === -1) return 1;
+        else if (moment1.isBefore(moment2)) return -1;
         else if (moment1.isAfter(moment2)) return 1;
         return 0;
     },
